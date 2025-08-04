c153477f691dca704dd79871db549d3598560a20
4wip <168364544+4wip@users.noreply.github.com>
2025-08-04 17:52:23 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, RichPresence } = require('discord.js-selfbot-v13');
+const client = new Client();
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+
+    const status = new RichPresence(client)
+    .setApplicationId('ID APP')
+    .setName('')
+    .setDetails('DESC')
+    .setAssetsLargeImage("URL") 
+    .setAssetsLargeText('Texte quand on survole l'image')
+    .addButton('', 'URL')
+    .addButton('', 'URL')
+    .setPlatform('desktop')
+    .setStartTimestamp(Date.now())
+  client.user.setPresence({ activities: [status] });
+})
+
+client.login('');   
