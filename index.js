2200cccaf8547f87b0d0758c5b74bd066bdd3596
4wip <168364544+4wip@users.noreply.github.com>
2025-08-25 14:54:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ const client = new Client();
 
 client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
+    const getExtendURL = await RichPresence.getExternal(
+    client,
+    '367827983903490050',
+    'https://i.ibb.co/nN1Zv00k/1000004180.jpg',
+  );
 
     const status = new RichPresence(client)
-    .setApplicationId('ID APP')
-    .setName('')
-    .setAssetsLargeImage("URL") 
-    .setAssetsLargeText('Texte quand on survole l'image')
-    .addButton('', 'URL')
-    .addButton('', 'URL')
+    .setApplicationId('367827983903490050')
+    .setType('COMPETING')
+    .setName('‎‎  ')
+    .setDetails('‎‎  ')
+    .setState('‎‎  ')
+    .setAssetsLargeImage(getExtendURL[0].external_asset_path) 
+  //  .setAssetsLargeText('?')
+    .addButton('AniList', 'https://anilist.co/user/Pistachee')
     .setPlatform('desktop')
   client.user.setPresence({ activities: [status] });
+
 })
 
-client.login('');   
+client.login("");
